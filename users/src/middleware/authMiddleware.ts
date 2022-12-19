cdd930b65e452622ac81dc9b24ee7e3d47da7fb4
CristianA <cris4599al@gmail.com>
2022-12-20 01:23:37 +0200

    auth-middleware

---
+++ users/src/middleware/authMiddleware.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const getSession = (req: express.Request, res: express.Response) => {
+  const { user } = req.session;
+
+  if (!user) {
+    return res.status(401).json({ status: 'fail', message: 'no session' });
+  }
+  return res.status(200).json({ status: 'success', data: req.session });
+};
