2e232cdd642c93323128d033435bddba64ae85fd
CristianA <cris4599al@gmail.com>
2022-12-20 01:10:24 +0200

    added mongo session storage

--- users/src/controllers/authController.ts
+++ users/src/controllers/authController.ts
@@ -2,11 +2,20 @@ import express from 'express';
 import User from '../models/user';
 import bcrypt from 'bcryptjs';
 
+import session from 'express-session';
+
+declare module 'express-session' {
+  export interface SessionData {
+    user: { [key: string]: any };
+  }
+}
+
 export const signUp = async (req: express.Request, res: express.Response) => {
   const { username, password } = req.body;
   try {
     const hashpassword = await bcrypt.hash(password, 12);
     const newUser = await User.create({ username, password: hashpassword });
+    req.session.user = newUser;
     res.status(201).json({
       status: 'success',
       data: { user: newUser },
@@ -25,18 +34,21 @@ export const login = async (req: express.Request, res: express.Response) => {
         status: 'fail',
         message: 'user not found',
       });
+      return;
     }
 
     const isCorrect = bcrypt.compare(password, user.password);
 
     if (isCorrect) {
+      req.session.user = user;
       res.status(200).json({ status: 'success' });
     } else {
       res
         .status(400)
         .json({ status: 'fail', message: 'incorrect username or password' });
     }
-  } catch {
+  } catch (e) {
+    console.error(e);
     res.status(400).json({ status: 'fail' });
   }
 };
--- users/src/index.ts
+++ users/src/index.ts
@@ -5,20 +5,25 @@ import {
   MONGO_PASSWORD,
   MONGO_PORT,
   MONGO_USER,
+  SESSION_SECRET,
 } from './config/config';
 import router from './routes/userRoutes';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 const userRouter = router;
 
 const app = express();
 const port = process.env.PORT || 3000; // default port to listen
 
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
 const connectWithRetry = () => {
   mongoose
-    .connect(
-      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-    )
-    .then(() => console.log('succesfully connected to DB'))
+    .connect(mongoUrl)
+    .then(() => {
+      console.log('succesfully connected to DB');
+    })
     .catch((e) => {
       console.log(e);
       setTimeout(connectWithRetry, 5000);
@@ -26,6 +31,23 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    store: MongoStore.create({
+      mongoUrl: mongoUrl,
+    }),
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: false,
+      secure: false,
+      httpOnly: true,
+      maxAge: 30000000,
+    },
+  })
+);
 
 app.use(express.json());
 
